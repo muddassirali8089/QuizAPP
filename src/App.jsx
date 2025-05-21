842d76200b14d8715aee60f4c3bc3b0f8e7cb6a7
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-05-22 01:08:47 +0500

    get the data from the fake api and store the data in the state in using reducer
    get the data from the fake api and store the data in the state in using reducer and handle the error action

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-
-
-
+import { useEffect, useReducer } from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 
-function App(){
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.data, status: "ready" };
+      case "dataFailed":
+        return{
+          ...state , status: "error"
+        }
 
-  return(
-   <div className="app">
-   <Header/>
-   <Main>
-    <p>1/15</p>
-    <p>Question?</p>
-   </Main>
-   </div>
-  )
+    default:
+      throw new Error("invalid action..");
+  }
+}
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  // "loading" , "error" , "ready" , "active" , "finished"
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", data: data }))
+      .catch((error) => dispatch({type:"dataFailed"}));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
 export default App;
