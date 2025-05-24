4ae907b46749489bb2e61d00941ea5ee8bcb4cf0
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-05-25 01:42:22 +0500

    update the tittle
    update the tittle

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className='app-header'>
-      <img src='../../public/ReactLogo.png' alt='React logo' />
+      <img src='ReactLogo.png' alt='React logo' />
       <h1>The React Quiz</h1>
     </header>
   );
